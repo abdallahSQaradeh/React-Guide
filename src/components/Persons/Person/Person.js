c56dacf17ee7ffbd64a710066430457152741179
Abdallah Qaradeh <40641982+abdallahSQaradeh@users.noreply.github.com>
2020-06-24 18:51:45 +0300

    Higher order component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classes from "./Person.css";
 import Radium from "radium";
 import styled from "styled-components";
+import Aux from "../../../hoc/Auxiliary";
 
 class Person extends Component {
   // !const random = Math.random();
@@ -38,18 +39,20 @@ class Person extends Component {
   };
   render() {
     console.log("[Person.js] Render");
-    return [
-      <p key="i1" onClick={this.props.click}>
-        I'm {this.props.name} and i am {this.props.age} !years old
-      </p>,
-      <p key="i2">{this.props.children}</p>,
-      <input
-        key="i3"
-        type="text"
-        onChange={this.props.changed}
-        value={this.props.name}
-      />,
-    ];
+    return (
+      <Aux>
+        <p key="i1" onClick={this.props.click}>
+          I'm {this.props.name} and i am {this.props.age} !years old
+        </p>
+        <p key="i2">{this.props.children}</p>,
+        <input
+          key="i3"
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Aux>
+    );
   }
   componentDidUpdate() {
     console.log("[Person.js] componentDidUpdate");
